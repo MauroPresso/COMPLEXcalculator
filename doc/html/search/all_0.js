ebefc69f81d19729ec2e855605d56339d7b54fdf
MauroPresso <mauro.presso@gmail.com>
2025-01-04 13:27:31 -0300

    Agrega la documentación generada por Doxygen

---
+++ doc/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['num_5fcomplejo_0',['num_complejo',['../structnum__complejo.html',1,'']]]
+];
---
+++ doc/html/search/variables_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['parte_5fimaginaria_0',['parte_imaginaria',['../structnum__complejo.html#a44d24f7944b6f5aee048cfb6fe8f0814',1,'num_complejo']]],
+  ['parte_5freal_1',['parte_real',['../structnum__complejo.html#ab65b445a90cc6f95a3caba657f56bc0d',1,'num_complejo']]]
+];
